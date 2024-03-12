98ab547c4b9f86c98131273df2f70bae92a1b7d5
BakuEmilov <emilovbaku530@gmail.com>
2024-03-13 01:35:17 +0600

    added new pages

---
+++ src/app/friends/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Frients",
+};
+
+const FriendsPage = () => {
+  return <div>FriendsPage</div>;
+};
+
+export default FriendsPage;
---
+++ src/app/settings/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Setting",
+};
+const SettingPage = () => {
+  return <div>SettingPage</div>;
+};
+
+export default SettingPage;
